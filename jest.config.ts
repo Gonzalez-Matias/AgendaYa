26b4d8c4d3f49f4bc3b6f10698f5855c626db6f2
JPMANUCHA <juampi_manucha@hotmail.com>
2026-06-17 20:03:32 -0300

    feature: agregar reporte HTML de tests en GitHub Actions

--- jest.config.ts
+++ jest.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
   },
   verbose: true,
   testTimeout: 30000,
+  reporters: [
+    'default',
+    ['jest-html-reporter', {
+      pageTitle: 'AgendaYa - Test Report',
+      outputPath: './test-report.html',
+      includeFailureMsg: true,
+      includeSuiteFailure: true,
+      dateFormat: 'dd-mm-yyyy HH:MM:ss',
+      sort: 'status',
+      hidePassed: false,
+      hidePending: false,
+      hideSkipped: false,
+    }]
+  ],
 };
 
 export default config;
